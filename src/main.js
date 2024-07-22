2f8eba9c5c8390c737d3e106ad155d6f646ad9b6
choivez <118510662+choivez@users.noreply.github.com>
2024-07-22 17:08:45 +0900

    Observer-part2

--- src/main.js
+++ src/main.js
@@ -1,27 +1,43 @@
 'use strict';
 
-// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
-const header = document.querySelector('.header');
-const headerHeight = header.offsetHeight;
-document.addEventListener('scroll', () => {
-    if (window.scrollY > headerHeight) {
-        header.classList.add('header--dark');
-    } else {
-        header.classList.remove('header--dark');
+function headerStyleOnScroll() {
+    const header = document.querySelector('.header');
+    const headerHeight = header.offsetHeight;
+    
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > headerHeight) {
+            header.classList.add('header--dark');
+        } else {
+            header.classList.remove('header--dark');
+        }
+    });
+}
+
+function homeFadeOutOnScroll(element) {
+    if (!element) {
+        return;
     }
-});
 
-// Home 섹션을 아래로 스크롤 시 투명하게 처리함
-const home = document.querySelector('.home__container');
-const homeHeight = home.offsetHeight;
-let opacity = 1;
-document.addEventListener('scroll', () => {
-    home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+    const elem = document.querySelector(element);
+    const distanceToTop = window.scrollY + elem.getBoundingClientRect().top;
+    const elemHeight = elem.offsetHeight;
+    const scrollTop = document.documentElement.scrollTop;
 
-// Arrow up 버튼을 아래로 스크롤 시 투명하게 처리 함
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
+    let opacity = 1;
+
+    if (scrollTop > distanceToTop) {
+        opacity = 1 - ((scrollTop - distanceToTop) / elemHeight);
+    }
+
+    if (opacity >= 0) {
+        elem.style.opacity = opacity;
+    }
+}
+
+function asideArrowShowOnScroll() {
+    const home = document.querySelector('#home');
+    const homeHeight = home.offsetHeight;
+    const arrowUp = document.querySelector('.arrow-up');
     if(window.scrollY > homeHeight / 2) {
         arrowUp.style.opacity = 1;
         arrowUp.style.visibility = 'visible';
@@ -29,17 +45,29 @@ document.addEventListener('scroll', () => {
         arrowUp.style.opacity = 0;
         arrowUp.style.visibility = 'hidden';
     }
-});
+}
 
-// Navbar 토글 버튼 클릭 처리
-const navbarMenu = document.querySelector('.header__menu');
-const navbarToggle = document.querySelector('.header__toggle');
-navbarToggle.addEventListener('click', () => {
-    navbarMenu.classList.toggle('open');
-});
+function menuBtnOnClick(event) {
+    const headerNav = document.querySelector('.header__nav');
 
-// Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
-navbarMenu.addEventListener('click', () => {
-    navbarMenu.classList.remove('open');
-});
+    if (event.target.tagName === 'I' && event.target.className === 'fa-solid fa-bars') {
+        headerNav.classList.toggle('nav__show');
+    }
+    if (event.target.tagName === 'A' && (event.target.className === 'header__menu__item' || event.target.className === 'header__menu__item active')) {
+        headerNav.classList.remove('nav__show');
+    }
 
+}
+
+function scrollHandler() {
+    headerStyleOnScroll();
+    homeFadeOutOnScroll('.home__container');
+    asideArrowShowOnScroll();
+}
+
+function clickEventHandler(e) {
+    menuBtnOnClick(e);
+}
+
+window.addEventListener('scroll', scrollHandler);
+window.addEventListener('click', clickEventHandler);
--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,7 @@ const projects = document.querySelectorAll('.project');
 const projectsContainer = document.querySelector('.projects');
 categories.addEventListener('click', (event) => {
     const filter = event.target.dataset.category;
-    if(filter == null) {
+    if (filter == null) {
         return;
     }
     handleActiveSelection(event.target);
@@ -20,14 +20,14 @@ function handleActiveSelection(target) {
 }
 
 function filterProjects(filter) {
-    projects.forEach((project) => {
-        if( filter === 'all' || filter === project.dataset.type) {
+    projects.forEach((project)=>{
+        if (filter === 'all' || filter === project.dataset.type) {
             project.style.display = 'block';
         } else {
             project.style.display = 'none';
         }
     });
-    
+
     projectsContainer.classList.add('anim-out');
     setTimeout(() => {
         projectsContainer.classList.remove('anim-out');
